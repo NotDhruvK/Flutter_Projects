7b21dd3391b2a0c28e40630836e775095a21d0ab
NotDhruvK <dhruvadam@gmail.com>
2026-02-10 23:12:18 +0530

    tweak: added image change on submit

--- hangman/lib/screens/playpage.dart
+++ hangman/lib/screens/playpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hangman/data/words.dart';
 
 class PlayPage extends StatefulWidget {
   const PlayPage({super.key});
@@ -8,8 +9,59 @@ class PlayPage extends StatefulWidget {
 }
 
 class _PlayPageState extends State<PlayPage> {
+  // C L A S S    V A R I A B L E S
+  final String word = getWord();
+  final _guessController = TextEditingController();
+  var tries = 1;
+
+  // C L A S S    M E T H O D S
+  // TODO: Implement a _getHint function
+  @override
+  void dispose() {
+    _guessController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hangman'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(word),
+              const SizedBox(
+                height: 16,
+                width: double.infinity,
+              ),
+              Image.asset('assets/$tries.jpeg'),
+              const SizedBox(height: 16),
+              const Text(
+                'Your Guess',
+                style: TextStyle(fontSize: 16),
+              ),
+              TextField(
+                decoration: const InputDecoration(
+                  fillColor: Colors.grey,
+                ),
+                textAlign: TextAlign.center,
+                controller: _guessController,
+                autofocus: true,
+                onSubmitted: (value) {
+                  setState(() {
+                    tries++;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
